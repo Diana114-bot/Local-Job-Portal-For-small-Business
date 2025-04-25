d680741232b86434635f08eb681c00d0a9da3d7c
Holy-21B <mosupiboitshepo@gmail.com>
2025-04-25 21:47:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import ForgotPassword from './pages/ForgotPassword';
 import EmployerDashboardPage from './pages/employer/EmployerDashboardPage';
+import Profile from './components/dashboard/Profile';
 import MyJobs from './components/dashboard/MyJobs';
 import Settings from './components/dashboard/Settings';
+import NotificationPage from './components/dashboard/NotificationPage';
+
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
-       
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/jobs" element={<MyJobs/>} />
-        <Route path="/settings" element={<Settings/>} />
-        
+        {/* Nested Dashboard Routes */}
+        <Route path="/dashboard" element={<DashboardPage />}>
+  <Route path="profile" element={<Profile />} />
+  <Route path="myjobs" element={<MyJobs />} />
+  <Route path="settings" element={<Settings />} />
+  <Route path="NotificationPage" element={<NotificationPage />} />
+</Route>
         <Route path="/employer/login" element={<LoginPage />} />
         <Route path="/employer/register" element={<RegisterPage />} />
         <Route path="/employer/forgot-password" element={<ForgotPassword />} />
